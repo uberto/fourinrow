3b913aab7cdffcb5a133c3f43ca5a6ea4dab799e
uberto <uberto.gama@gmail.com>
2019-10-01 22:33:34 +0100

    enum prev and next

--- src/com/ubertob/fourinarow/Board.kt
+++ src/com/ubertob/fourinarow/Board.kt
@@ -28,29 +28,32 @@ object EmptyBoard : Board() {
 
 data class GameBoard(val prevBoard: Board, val column: Column) : Board() {
 
-    override val nextPlayer = prevBoard.nextPlayer.other()
+    val player = prevBoard.nextPlayer
 
-    val player = nextPlayer.other()
+    override val nextPlayer = player.other()
+
 
     override val moves: List<Move> = prevBoard.moves + Move(prevBoard.nextPlayer, column)
 
     val colMap =
         Column.values().map { c -> c to moves.filter { it.column == c }.map { it.player }.let { Pile(it) } }.toMap()
 
-    val currRow: Int = colMap[column]?.rows?.size ?: 0
+    val row: Row = Row.values()[colMap[column]?.rows?.size ?: 0]
 
-    fun playerInBoard(col: Column?, row: Row?): Player? = if (row == null || col == null) null else colMap[col]?.rows?.getOrNull(row.value)
+    fun playerInBoard(col: Column?, row: Row?): Player? =
+        if (row == null || col == null) null else colMap[col]?.rows?.getOrNull(row.ordinal)
 
-    private fun drawGrid(): String = (1..COL_HEIGHT)
-        .map { row -> colMap.values
-                .map { it.render(COL_HEIGHT - row ) }
+    private fun drawGrid(): String = Row.values()
+        .map { row ->
+            colMap.values
+                .map { it.render(6 - row.ordinal) }
                 .joinToString("|")
                 .let { "|$it|" }
         }
         .joinToString("\n")
 
     override fun show(): String = """
-Move number ${moves.size} player ${player} column ${column}
+Move number ${moves.size} player ${player} column ${column} row ${row}
         
 +-+-+-+-+-+-+-+
 ${drawGrid()}
@@ -60,7 +63,30 @@ ${drawGrid()}
         
 """
 
+    fun playerAtCoord(c: Coord): Player? = playerInBoard(c.col, c.row)
 
-    fun winner(): Player? = playerInBoard(column.prev(),  Row.fromInt(currRow -1))
+
+    private fun nullCoord(column: Column?, row: Row?): Coord? =
+        if (column != null && row != null)
+            Coord(column, row)
+        else
+            null
+
+    fun Coord.se(): Coord? = nullCoord(col.next(), row.prev())
+    fun Coord.ne(): Coord? = nullCoord(col.next(), row.next())
+    fun Coord.e(): Coord? = nullCoord(col.next(), row)
+    fun Coord.sw(): Coord? = nullCoord(col.prev(), row.prev())
+    fun Coord.nw(): Coord? = nullCoord(col.prev(), row.next())
+    fun Coord.w(): Coord? = nullCoord(col.prev(), row)
+    fun Coord.s(): Coord? = nullCoord(col, row.prev())
+
+    fun winner(): Player? = playerInBoard(column, row)
+//    move.s().s().s() >= 3
+//    move.ne().ne().ne() + move.sw().sw().sw() >= 3 //diagonal  move is +1
+//    move.nw().nw().nw() + move.se().se().se() >= 3 //diagonal  move is +1
+//    move.e().e().e() + move.w().w().w() >= 3 //horz  move is +1
+
 
 }
+
+data class Coord(val col: Column, val row: Row)
--- src/com/ubertob/fourinarow/Move.kt
+++ src/com/ubertob/fourinarow/Move.kt
@@ -1,27 +1,33 @@
 package com.ubertob.fourinarow
 
-val COL_HEIGHT = 6
+import java.lang.Exception
+
+
 
 data class Move(val player: Player, val column: Column)
 
-@Suppress("DataClassPrivateConstructor")
-data class Row private constructor(val value: Int) {
-    companion object {
-        fun fromInt(x: Int): Row? = if (x in 0.. COL_HEIGHT) Row(x) else null
-    }
-}
+
+data class NonValidMove(val column: String) : Exception()
+
+
+
 
 enum class Column{ a,b,c,d,e,f,g}
 
-fun Column.prev(): Column? = when (this) {
-    Column.a -> null
-    else -> Column.values()[this.ordinal - 1]
-}
+enum class Row{ r1,r2,r3,r4,r5,r6}
 
-fun Column.next(): Column? = when (this) {
-    Column.g -> null
-    else -> Column.values()[this.ordinal + 1]
-}
+inline fun <reified E: Enum<E>> Enum<E>.next(): E? =  if( ordinal == Column.values().size - 1)
+    null
+else
+    enumValues<E>()[ordinal + 1]
+
+inline fun <reified E: Enum<E>> Enum<E>.prev(): E? =  if( ordinal == 0)
+    null
+else
+    enumValues<E>()[ordinal - 1]
+
+
+
 
 data class Pile(val rows: List<Player>){
 
