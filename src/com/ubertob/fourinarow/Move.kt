d0a896b074805f6dd2a8ec168703cda104746475
uberto <uberto.gama@gmail.com>
2019-10-02 23:14:33 +0100

    tidy up

--- src/com/ubertob/fourinarow/Move.kt
+++ src/com/ubertob/fourinarow/Move.kt
@@ -1,5 +1,11 @@
 package com.ubertob.fourinarow
 
+enum class Player(val sign: Char) { One('X'), Two('O') }
+
+fun Player.other(): Player = when(this){
+    Player.One -> Player.Two
+    Player.Two -> Player.One
+}
 
 data class Move(val player: Player, val column: Column)
 
--- src/com/ubertob/fourinarow/Player.kt
+++
@@ -1,8 +0,0 @@
-package com.ubertob.fourinarow
-
-enum class Player(val sign: Char) { One('X'), Two('O') }
-
-fun Player.other(): Player = when(this){
-    Player.One -> Player.Two
-    Player.Two -> Player.One
-}
