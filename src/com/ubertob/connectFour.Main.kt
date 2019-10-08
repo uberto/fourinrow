66fdf469a681307356b5a7a5ce5e8fa83ca864b3
uberto <uberto.gama@gmail.com>
2019-10-08 21:04:15 +0100

    Starting point for live coding

---
+++ src/com/ubertob/connectFour.Main.kt
@@ -0,0 +1,19 @@
+package com.ubertob
+
+/*
+
+TODO:
+- show empty board
+- place one chip
+- pile multiple chips
+- add second player
+- show who is winning
+
+ */
+
+
+fun main() {
+
+    println("Ready to play?")
+
+}
