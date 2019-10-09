6c74ccbf3a79cd06d69749ff10c759d5331546b8
uberto <uberto.gama@gmail.com>
2019-10-09 10:18:19 +0100

    Add the introduction

--- src/com/ubertob/connectFour.Main.kt
+++ src/com/ubertob/connectFour.Main.kt
@@ -2,12 +2,47 @@ package com.ubertob
 
 /*
 
+
+
+
+                                                              __  _     _
+                                  | _    |   _ __  _| _ __     _)/ \/| (_|
+                                \_|(_|>< |__(_)| |(_|(_)| |   /__\_/ |  _|
+
+  _  __     _   _ _         _____                _
+ | |/ /    | | | (_)       |  __ \              | |
+ | ' / ___ | |_| |_ _ __   | |__) |__  __ _ _ __| |___
+ |  < / _ \| __| | | '_ \  |  ___/ _ \/ _` | '__| / __|
+ | . \ (_) | |_| | | | | | | |  |  __/ (_| | |  | \__ \
+ |_|\_\___/ \__|_|_|_| |_| |_|   \___|\__,_|_|  |_|___/
+
+
+            _________                                     __     _____
+            \_   ___ \  ____   ____   ____   ____   _____/  |_  /  |  |
+            /    \  \/ /  _ \ /    \ /    \_/ __ \_/ ___\   __\/   |  |_
+            \     \___(  <_> )   |  \   |  \  ___/\  \___|  | /    ^   /
+             \______  /\____/|___|  /___|  /\___  >\___  >__| \____   |
+                    \/            \/     \/     \/     \/          |__|
+
+
+
+      by Uberto Barbini
+      Twitter: @ramtop
+      Blog:    https://medium.com/@ramtop
+      GitHub:  https://github.com/uberto/fourinrow
+
+
+
+
+
 TODO:
 - show empty board
 - place one chip
 - pile multiple chips
 - add second player
-- show who is winning
+- show the winner with trivial rule
+(implement real rules)
+
 
  */
 
